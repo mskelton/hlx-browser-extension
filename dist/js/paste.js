382dac85bae00f885479697e49fc2f532e4e1b35
Mark Skelton <mdskelton99@gmail.com>
2022-02-23 13:08:13 -0600

    Finish

--- dist/js/paste.js
+++ dist/js/paste.js
@@ -1,11 +1,13 @@
-const container = document.querySelector(".buttonC")
-const button = document.createElement("button")
+;(function () {
+  const container = document.querySelector(".buttonC")
+  const button = document.createElement("button")
 
-button.textContent = "Paste"
-button.addEventListener("click", () => {
-  navigator.clipboard.readText().then((text) => {
-    window.wmks.sendInputString(text)
+  button.textContent = "Paste"
+  button.addEventListener("click", () => {
+    navigator.clipboard.readText().then((text) => {
+      window.wmks.sendInputString(text)
+    })
   })
-})
 
-container?.appendChild(button)
+  container?.appendChild(button)
+})()
