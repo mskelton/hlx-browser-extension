d3fa66f7461e7ad9f9fb94720c7e3eb44f451208
Mark Skelton <mdskelton99@gmail.com>
2022-07-09 16:32:38 -0500

    Handle keyboard

--- dist/js/paste.js
+++ dist/js/paste.js
@@ -2,12 +2,23 @@
   const container = document.querySelector(".buttonC")
   const button = document.createElement("button")
 
-  button.textContent = "Paste"
-  button.addEventListener("click", () => {
+  function paste() {
     navigator.clipboard.readText().then((text) => {
       window.wmks.sendInputString(text)
     })
-  })
+  }
 
+  button.textContent = "Paste"
+  button.addEventListener("click", paste)
   container?.appendChild(button)
+
+  function handleKeyDown(e) {
+    if (e.key === "v" && (e.metaKey || e.ctrlKey)) {
+      e.preventDefault()
+      e.stopPropagation()
+      paste()
+    }
+  }
+
+  document.addEventListener("keydown", handleKeyDown, true)
 })()
